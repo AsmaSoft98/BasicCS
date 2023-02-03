0226bba10b0f1fbd6b73ae1386fa5f25af42c7c9
Amna@Soft <12900826+AsmaSoft98@users.noreply.github.com>
2023-02-03 18:03:54 +0100

    Add files via upload

---
+++ CallByReference/Program.cs
@@ -0,0 +1,22 @@
+﻿
+public class Program
+{
+    // User defined function  
+    public void Show(ref int val)
+    {
+        val *= val; // Manipulating value  
+        Console.WriteLine("Value inside the show function " + val);
+    }
+
+    static void Main(string[] args)
+    {
+        int val = 50;
+        Program program = new Program();
+        Console.WriteLine("Value before calling the function " + val);
+
+        program.Show(ref val);
+        Console.WriteLine("Value after calling the function " + val);
+
+        Console.ReadKey();
+    }
+}

953a10e546654757c9a081f01f9c0cac2c43fef5
Amna@Soft <12900826+AsmaSoft98@users.noreply.github.com>
2023-02-03 18:39:19 +0100

    Add files via upload

---
+++ While/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int i = 1;
+
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= 10);
+        Console.ReadKey();
+    }
+}

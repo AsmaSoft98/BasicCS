798f70d830e63733c3a6af11e59a9d85677f8ffd
Sym Bli <SymBli@outlook.fr>
2023-02-03 11:23:08 +0100

    Initial commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        int x = 5;
+        int y = 8;
+        int z = x + y;
+        Console.WriteLine("Add Two Numbers：{0}+{1}={2}", x, y, z);
+
+        String str = "Name";
+        float f = 3.14f;
+        double d = 3.15;
+        bool boo = true;
+        Console.WriteLine("String:{0},\nfloat:{1},\ndouble:{2},\nbool:{3}", str, f, d, boo);
+        //escape characters are not recognized
+        Console.WriteLine(@"aaa\nbbb""
+                                bbb");
+        //output result：aaa\nbbb" new line bbb *define multiple lines
+
+        Console.ReadKey();        //any key to continue
+    }
+}
